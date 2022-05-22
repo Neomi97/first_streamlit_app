3b46ffe71319b100a6422befc7c70db68b405653
Neomi97 <105274065+Neomi97@users.noreply.github.com>
2022-05-22 15:15:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ except URLError as e:
 streamlit.header("The Fruit Load List Contains:")
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
-      my_cur.execute("SELECT * from fruit_load_list")
-      return my_cur.fetchall()
+            my_cur.execute("SELECT * from fruit_load_list")
+            return my_cur.fetchall()
 
 #add the button to load the fruit
 if streamlit.button('Get Fruit Load List'):
